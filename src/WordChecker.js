74792c31fb82a3de69c173994f6874079fd30180
datakun <me@kimjunu.com>
2022-01-29 00:15:22 +0900

    Update WordChecker.js

--- src/WordChecker.js
+++ src/WordChecker.js
@@ -1,6 +1,6 @@
 export async function searchWord(word) {
 	try {
-		const url = `https://stdict.korean.go.kr/api/search.do?&key=80E105230FCD3224499425D80C5B6037&req_type=json&q=${word}`;
+		const url = `https://stdict.korean.go.kr/api/search.do?&key=80E105230FCD3224499425D80C5B6037&req_type=json&type_search=search&q=${word}`;
 
 		// fetch get request
 		const response = await fetch(encodeURI(url), {
